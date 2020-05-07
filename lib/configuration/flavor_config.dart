b3248c87ace65d2157cc33559fce1dc25dd5a760
diego-rogel <diegorogel3d@hotmail.com>
2020-05-07 22:57:32 +0200

    adds flavor configurations

--- lib/configuration/flavor_config.dart
+++ lib/configuration/flavor_config.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:colored/configuration/flavor_values.dart';
 import 'flavor.dart';
 
 class FlavorConfig {
   factory FlavorConfig({
     @required Flavor flavor,
-    @required FlavorValues values,
     Color color = Colors.blue,
   }) =>
       _instance ??=
       FlavorConfig._internal(
-          flavor, flavor.toString().split('.').last, color, values);
+          flavor, flavor.toString().split('.').last, color);
 
-  FlavorConfig._internal(this.flavor, this.name, this.color, this.values);
+  FlavorConfig._internal(this.flavor, this.name, this.color);
+
+  final Flavor flavor;
+  final String name;
+  final Color color;
+  static FlavorConfig _instance;
 
   static FlavorConfig get instance => _instance;
 
   static bool isProduction() => _instance.flavor == Flavor.production;
 
   static bool isDevelopment() => _instance.flavor == Flavor.dev;
-
-  static bool isQA() => _instance.flavor == Flavor.qa;
-
-  final Flavor flavor;
-  final String name;
-  final Color color;
-  final FlavorValues values;
-  static FlavorConfig _instance;
 }
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,15 +2,16 @@ import 'package:colored/configuration/flavor.dart';
 import 'package:colored/configuration/flavor_config.dart';
 import 'package:colored/sources/app/colored.dart';
 import 'package:colored/sources/app/navigation/on_boarding_router.dart';
-import 'package:colored/sources/data/services/local_storage/shared_preferences.dart';
+import 'package:colored/sources/data/services/local_storage/mock_local_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  FlavorConfig(flavor: Flavor.production);
+  FlavorConfig(flavor: Flavor.dev);
 
   final initialRoute = await OnBoardingRouter.getInitialRoute(
-    const SharedPreferences(),
+    const MockLocalStorage(),
   );
 
   runApp(Colored(initialRoute: initialRoute));
