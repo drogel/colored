873e3bc01402e1651afc013cacf6ba64bcd67ec1
drogel <diegorogel3d@hotmail.com>
2020-06-09 11:10:50 +0200

    simplifies HSV and HSL converters

--- lib/sources/presentation/layouts/converter/converter_body_layout.dart
+++ lib/sources/presentation/layouts/converter/converter_body_layout.dart
@@ -3,7 +3,7 @@ import 'package:colored/sources/domain/view_models/converter/converter_data.dart
 import 'package:colored/sources/app/styling/padding/padding_data.dart';
 import 'package:colored/sources/presentation/widgets/containers/overlay_container.dart';
 import 'package:colored/sources/presentation/widgets/containers/swiping_cross_fade.dart';
-import 'package:colored/sources/presentation/widgets/sliders/color_sliders.dart';
+import 'package:colored/sources/presentation/widgets/pickers/color_sliders.dart';
 import 'package:colored/sources/presentation/widgets/buttons/dropdown_format_button.dart';
 import 'package:flutter/material.dart';
 
