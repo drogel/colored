2adc38efa42d9f7efc3d4641b28e2ba58a764022
Diego Rogel <50502687+drogel@users.noreply.github.com>
2020-06-18 20:18:34 +0200

    separates picker BLoC module from converter module (#23)
    * adds icons to picker tab bar
    
    * fixes build issues

--- lib/sources/presentation/layouts/converter/converter_body_layout.dart
+++ lib/sources/presentation/layouts/converter/converter_body_layout.dart
@@ -2,7 +2,7 @@ import 'package:colored/sources/app/styling/padding/padding_scheme.dart';
 import 'package:colored/sources/domain/data_models/format.dart';
 import 'package:colored/sources/domain/view_models/converter/converter_data.dart';
 import 'package:colored/sources/app/styling/padding/padding_data.dart';
-import 'package:colored/sources/presentation/layouts/converter/converter_picker.dart';
+import 'package:colored/sources/presentation/layouts/picker/converter_picker.dart';
 import 'package:colored/sources/presentation/widgets/containers/overlay_container.dart';
 import 'package:colored/sources/presentation/widgets/containers/swiping_cross_fade.dart';
 import 'package:colored/sources/presentation/widgets/buttons/dropdown_format_button.dart';
