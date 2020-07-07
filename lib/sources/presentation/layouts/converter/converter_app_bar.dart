f021e7bc005e6bd863cda65ad39742532b564c46
drogel <diegorogel3d@hotmail.com>
2020-07-08 00:16:00 +0200

    refactors layouts to use cross switchers when possible

--- lib/sources/presentation/layouts/converter/converter_app_bar.dart
+++ lib/sources/presentation/layouts/converter/converter_app_bar.dart
@@ -1,24 +1,25 @@
 import 'package:colored/sources/presentation/layouts/names_list/color_names_app_bar.dart';
 import 'package:colored/sources/presentation/layouts/naming/naming_layout.dart';
-import 'package:colored/sources/presentation/widgets/animations/default_animated_switcher.dart';
+import 'package:colored/sources/presentation/widgets/animations/cross_switchers/cross_animated_switcher.dart';
+import 'package:colored/sources/presentation/widgets/animations/cross_switchers/cross_switcher_state.dart';
 
 import 'package:flutter/material.dart';
 
 class ConverterAppBar extends StatelessWidget implements PreferredSizeWidget {
   const ConverterAppBar({
-    @required this.isSearching,
+    @required this.searchingState,
     this.onSearchStateChange,
     Key key,
   }) : super(key: key);
 
   final void Function() onSearchStateChange;
-  final bool isSearching;
+  final CrossSwitcherState searchingState;
 
   @override
-  Widget build(BuildContext context) => DefaultAnimatedSwitcher(
-        child: isSearching
-            ? ColorNamesAppBar(onBackPressed: onSearchStateChange)
-            : NamingLayout(onSearchPressed: onSearchStateChange),
+  Widget build(BuildContext context) => CrossAnimatedSwitcher(
+        state: searchingState,
+        firstChild: NamingLayout(onSearchPressed: onSearchStateChange),
+        secondChild: ColorNamesAppBar(onBackPressed: onSearchStateChange),
       );
 
   @override
--- lib/sources/presentation/layouts/converter/converter_layout.dart
+++ lib/sources/presentation/layouts/converter/converter_layout.dart
@@ -7,7 +7,8 @@ import 'package:colored/sources/presentation/layouts/converter/converter_body_la
 import 'package:colored/sources/presentation/layouts/names_list/names_list_layout.dart';
 import 'package:colored/sources/presentation/layouts/naming/naming_error_row.dart';
 import 'package:colored/sources/presentation/layouts/transformer/transformer_layout.dart';
-import 'package:colored/sources/presentation/widgets/animations/default_transition_switcher.dart';
+import 'package:colored/sources/presentation/widgets/animations/cross_switchers/cross_switcher_state.dart';
+import 'package:colored/sources/presentation/widgets/animations/cross_switchers/cross_transition_switcher.dart';
 import 'package:flutter/material.dart';
 
 class ConverterLayout extends StatefulWidget {
@@ -18,26 +19,28 @@ class ConverterLayout extends StatefulWidget {
 }
 
 class _ConverterLayoutState extends State<ConverterLayout> {
-  bool isSearching = false;
+  CrossSwitcherState searchingState = CrossSwitcherState.showFirst;
 
   @override
   Widget build(BuildContext context) => Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: ConverterAppBar(
-          isSearching: isSearching,
+          searchingState: searchingState,
           onSearchStateChange: _updateSearchingState,
         ),
-        body: DefaultTransitionSwitcher(
-          child: isSearching
-              ? NamesListLayout(onColorCardPressed: _onColorCardPressed)
-              : const ConnectivityLayout(
-                  body: ConverterBodyLayout(background: TransformerLayout()),
-                  child: ConnectivityBar(child: NamingErrorRow()),
-                ),
+        body: CrossTransitionSwitcher(
+          state: searchingState,
+          firstChild: const ConnectivityLayout(
+            body: ConverterBodyLayout(background: TransformerLayout()),
+            child: ConnectivityBar(child: NamingErrorRow()),
+          ),
+          secondChild: NamesListLayout(onColorCardPressed: _onColorCardPressed),
         ),
       );
 
-  void _updateSearchingState() => setState(() => isSearching = !isSearching);
+  void _updateSearchingState() => setState(
+        () => searchingState = searchingState.switched(),
+      );
 
   void _onColorCardPressed(Color color) {
     final selection = ColorSelection.fromColor(color);
