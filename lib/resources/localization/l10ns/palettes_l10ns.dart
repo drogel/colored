6cc5542a70ad5297203aa3fcd50a131c6353beda
drogel <diegorogel3d@hotmail.com>
2020-08-21 12:51:09 +0200

    adds palette notifier

--- lib/resources/localization/l10ns/palettes_l10ns.dart
+++ lib/resources/localization/l10ns/palettes_l10ns.dart
@@ -6,11 +6,14 @@ class PalettesL10ns extends LocaleCodeAware {
   static final Map<String, Map<String, String>> _values = {
     'en': {
       'pageTitle' : 'Palettes',
+      'noPalettesFound': 'Oops! No palettes seem to be matching that name.',
     },
     "es": {
       'pageTitle' : 'Paletas',
+      'noPalettesFound': 'Ups! No parece haber paletas con ese nombre.',
     }
   };
 
   String get pageTitle => _values[localeCode]['pageTitle'];
+  String get noPalettesFound => _values[localeCode]['noPalettesFound'];
 }
---
+++ lib/sources/presentation/layouts/palettes_list/palettes_list_layout.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PalettesListLayout extends StatelessWidget {
+  const PalettesListLayout({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container();
+}
