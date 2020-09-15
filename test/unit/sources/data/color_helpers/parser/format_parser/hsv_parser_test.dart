17bce0f46b423c2d380f57e856b4b266823db761
drogel <diegorogel3d@hotmail.com>
2020-09-15 15:32:00 +0200

    test: adds tests for hsv format parser
    fix: fixes optional hsl prefix incorrectly placed on hsl format parser

---
+++ test/unit/sources/data/color_helpers/parser/format_parser/hsv_parser_test.dart
@@ -0,0 +1,89 @@
+import 'package:colored/sources/data/color_helpers/parser/format_parser/format_parser.dart';
+import 'package:colored/sources/data/color_helpers/parser/format_parser/hsv_parser.dart';
+import 'package:colored/sources/domain/data_models/color_selection.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  FormatParser hsvParser;
+
+  setUp(() {
+    hsvParser = HsvParser();
+  });
+
+  tearDown(() {
+    hsvParser = null;
+  });
+
+  group("Given a HsvParser", () {
+    group("when hasMatch is called", () {
+      test("true is returned if input String is an hsv color code", () {
+        expect(hsvParser.hasMatch("174°, 42%, 83%"), isTrue);
+        expect(hsvParser.hasMatch("(174°, 42%, 83%)"), isTrue);
+        expect(hsvParser.hasMatch("174°, 42%%, 83%"), isTrue);
+        expect(hsvParser.hasMatch("174° 42%, 83%"), isTrue);
+        expect(hsvParser.hasMatch("174° 42% 83%"), isTrue);
+        expect(hsvParser.hasMatch("hsv(174° 42% 83%)"), isTrue);
+        expect(hsvParser.hasMatch("174.1°, 42.3%, 83.6%"), isTrue);
+        expect(hsvParser.hasMatch("174, 42%, 83%"), isTrue);
+        expect(hsvParser.hasMatch("174°, 42, 83%"), isTrue);
+        expect(hsvParser.hasMatch("174°, 42%, 83"), isTrue);
+        expect(hsvParser.hasMatch("174°, 42, 83%"), isTrue);
+        expect(hsvParser.hasMatch("174°, 42%, 83"), isTrue);
+        expect(hsvParser.hasMatch("174, 42%, 83%"), isTrue);
+        expect(hsvParser.hasMatch("360°, 0%, 100%"), isTrue);
+        expect(hsvParser.hasMatch("724°, 0%, 100%"), isTrue);
+        expect(hsvParser.hasMatch("360°, 120%, 100%"), isTrue);
+        expect(hsvParser.hasMatch("360°, 100%, 200%"), isTrue);
+        expect(hsvParser.hasMatch("360, 0, 100"), isTrue);
+        expect(hsvParser.hasMatch("360.0, 0.0, 100.0"), isTrue);
+        expect(hsvParser.hasMatch("174°, 42%, 83%, 100%"), isTrue);
+        expect(hsvParser.hasMatch("174°, 42%, 83% this is a test"), isTrue);
+      });
+
+      test("false is returned if input String is null", () {
+        expect(hsvParser.hasMatch(null), isFalse);
+      });
+
+      test("false is returned if input String is not an hsv color code", () {
+        expect(hsvParser.hasMatch(""), isFalse);
+        expect(hsvParser.hasMatch("2w"), isFalse);
+        expect(hsvParser.hasMatch("hello"), isFalse);
+        expect(hsvParser.hasMatch("hsl(174° 42% 83%)"), isFalse);
+        expect(hsvParser.hasMatch("rgb(174 42 83)"), isFalse);
+        expect(hsvParser.hasMatch("#232323"), isFalse);
+      });
+    });
+
+    group("when parse is called", () {
+      test("then the ColorSelection for the hex code is retrieved", () {
+        expect(
+          hsvParser.parse("0 100 50"),
+          ColorSelection(r: 0.5, g: 0, b: 0),
+        );
+        expect(hsvParser.parse("0, 0, 100"), ColorSelection(r: 1, g: 1, b: 1));
+        expect(
+          hsvParser.parse("0°, 100%, 50%"),
+          ColorSelection(r: 0.5, g: 0, b: 0),
+        );
+        expect(
+          hsvParser.parse("300°, 100%, 50%"),
+          ColorSelection(r: 0.5, g: 0, b: 0.5),
+        );
+      });
+
+      test("then an assertion error is thrown on null input string", () {
+        expect(
+          () => hsvParser.parse(null),
+          throwsA(isA<AssertionError>()),
+        );
+      });
+
+      test("then an assertion error is thrown on empty input string", () {
+        expect(
+          () => hsvParser.parse(""),
+          throwsA(isA<AssertionError>()),
+        );
+      });
+    });
+  });
+}
