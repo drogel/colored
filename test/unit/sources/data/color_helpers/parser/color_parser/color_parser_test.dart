4b940116f14cffedaee940e81ac7c3ef20ad141f
drogel <diegorogel3d@hotmail.com>
2020-09-15 16:26:55 +0200

    test: adds tests for color parser
    fix: fixes the way color parser decides which parser to use depending on
    the format

---
+++ test/unit/sources/data/color_helpers/parser/color_parser/color_parser_test.dart
@@ -0,0 +1,71 @@
+import 'package:colored/sources/data/color_helpers/parser/color_parser/color_parser.dart';
+import 'package:colored/sources/data/color_helpers/parser/color_parser/parser.dart';
+import 'package:colored/sources/data/color_helpers/parser/format_parser/hex_parser.dart';
+import 'package:colored/sources/data/color_helpers/parser/format_parser/hsl_parser.dart';
+import 'package:colored/sources/data/color_helpers/parser/format_parser/hsv_parser.dart';
+import 'package:colored/sources/data/color_helpers/parser/format_parser/rgb_parser.dart';
+import 'package:colored/sources/domain/data_models/format.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  Parser colorParser;
+
+  setUp(() {
+    colorParser = const ColorParser(formatParsers: {});
+  });
+
+  tearDown(() {
+    colorParser = null;
+  });
+
+  group("Given a ColorParser", () {
+    group("when constructed", () {
+      test("an assertion error is thrown if formatParsers is null", () {
+        expect(
+          () => ColorParser(formatParsers: null),
+          throwsA(isA<AssertionError>()),
+        );
+      });
+    });
+
+    group("when isStringOfFormat is called", () {
+      test("an UnimplmementedError is thrown if formatParsers is empty", () {
+        expect(
+          () => colorParser.isStringOfFormat("test", Format.hex),
+          throwsA(isA<UnimplementedError>()),
+        );
+      });
+
+      test("an error is thrown if the given format is not found", () {
+        final rgbOnlyParser = ColorParser(formatParsers: {
+          Format.rgb: RgbParser(),
+        });
+
+        expect(
+          () => rgbOnlyParser.isStringOfFormat("test", Format.hex),
+          throwsA(isA<UnimplementedError>()),
+        );
+      });
+    });
+
+    group("when parseFromFormat is called", () {
+      test("an UnimplmementedError is thrown if formatParsers is empty", () {
+        expect(
+          () => colorParser.parseFromFormat("test", Format.hex),
+          throwsA(isA<UnimplementedError>()),
+        );
+      });
+
+      test("an error is thrown if the given format is not found", () {
+        final rgbOnlyParser = ColorParser(formatParsers: {
+          Format.rgb: RgbParser(),
+        });
+
+        expect(
+          () => rgbOnlyParser.parseFromFormat("test", Format.hex),
+          throwsA(isA<UnimplementedError>()),
+        );
+      });
+    });
+  });
+}
