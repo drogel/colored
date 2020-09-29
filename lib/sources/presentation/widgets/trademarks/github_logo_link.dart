ae1bc5ed699471abd44b85568087dd45ce1567f9
drogel <diegorogel3d@hotmail.com>
2020-09-29 19:13:56 +0200

    feat: adds a color on hover for github logo link widget

--- lib/sources/presentation/widgets/trademarks/github_logo_link.dart
+++ lib/sources/presentation/widgets/trademarks/github_logo_link.dart
@@ -3,11 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:colored/resources/asset_paths.dart' as asset_paths;
 import 'package:url_launcher/url_launcher.dart';
 
-class GithubLogoLink extends StatelessWidget {
+class GithubLogoLink extends StatefulWidget {
   const GithubLogoLink({this.height, this.width, Key key}) : super(key: key);
 
   final double height;
   final double width;
+
+  @override
+  _GithubLogoLinkState createState() => _GithubLogoLinkState();
+}
+
+class _GithubLogoLinkState extends State<GithubLogoLink> {
+  Color _currentLogoColor;
+  Color _idleLogoColor;
+  Color _hoveringLogoColor;
+
+  @override
+  void didChangeDependencies() {
+    _idleLogoColor = Theme.of(context).colorScheme.onError;
+    _hoveringLogoColor = Theme.of(context).colorScheme.secondary;
+    _currentLogoColor = _idleLogoColor;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +33,12 @@ class GithubLogoLink extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: padding.medium.left),
       child: InkWell(
         onTap: _launchGithubURL,
+        onHover: _updateLogoColor,
         child: Image.asset(
           asset_paths.githubLogo,
-          height: height,
-          width: width,
+          height: widget.height,
+          width: widget.width,
+          color: _currentLogoColor,
         ),
       ),
     );
@@ -31,4 +50,8 @@ class GithubLogoLink extends StatelessWidget {
       await launch(url);
     }
   }
+
+  void _updateLogoColor(bool isHovering) => isHovering
+      ? setState(() => _currentLogoColor = _hoveringLogoColor)
+      : setState(() => _currentLogoColor = _idleLogoColor);
 }
