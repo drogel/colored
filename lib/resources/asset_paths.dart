cfc21d850ee49a4ed1a36e911a507716100fe0a4
drogel <diegorogel3d@hotmail.com>
2020-09-29 18:48:07 +0200

    feat: adds link to github colored repository

--- lib/resources/asset_paths.dart
+++ lib/resources/asset_paths.dart
@@ -10,3 +10,5 @@ const colors = "$_data/colors.json";
 const colorSuggestions = "$_data/color_suggestions.json";
 const palettes = "$_data/palettes.json";
 const paletteSuggestions = "$_data/palette_suggestions.json";
+
+const githubColoredLink = "https://github.com/drogel/colored";
--- lib/sources/presentation/widgets/trademarks/github_logo_link.dart
+++ lib/sources/presentation/widgets/trademarks/github_logo_link.dart
@@ -1,6 +1,7 @@
 import 'package:colored/sources/app/styling/padding/padding_data.dart';
 import 'package:flutter/material.dart';
 import 'package:colored/resources/asset_paths.dart' as asset_paths;
+import 'package:url_launcher/url_launcher.dart';
 
 class GithubLogoLink extends StatelessWidget {
   const GithubLogoLink({this.height, this.width, Key key}) : super(key: key);
@@ -13,11 +14,21 @@ class GithubLogoLink extends StatelessWidget {
     final padding = PaddingData.of(context).paddingScheme;
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: padding.medium.left),
-      child: Image.asset(
-        asset_paths.githubLogo,
-        height: height,
-        width: width,
+      child: InkWell(
+        onTap: _launchGithubURL,
+        child: Image.asset(
+          asset_paths.githubLogo,
+          height: height,
+          width: width,
+        ),
       ),
     );
+  }
+
+  Future<void> _launchGithubURL() async {
+    const url = asset_paths.githubColoredLink;
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
   }
 }
