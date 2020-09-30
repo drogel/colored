f72673a18e1720a186a83d2a826702daeb9ee8ef
drogel <diegorogel3d@hotmail.com>
2020-09-30 17:13:16 +0200

    refactor: makes the url launcher logic testable

--- lib/sources/data/services/url_launcher/safe_url_launcher.dart
+++ lib/sources/data/services/url_launcher/safe_url_launcher.dart
@@ -3,16 +3,25 @@ import 'package:flutter/foundation.dart';
 import 'package:url_launcher/url_launcher.dart' as launcher;
 
 class SafeUrlLauncher implements UrlLauncher {
-  const SafeUrlLauncher({@required String url})
-      : assert(url != null),
+  const SafeUrlLauncher({
+    @required String url,
+    Future<void> Function(String) launchAction = launcher.launch,
+    Future<bool> Function(String) canLaunchCheck = launcher.canLaunch,
+  })  : assert(url != null),
+        assert(launchAction != null),
+        assert(canLaunchCheck != null),
+        _canLaunch = canLaunchCheck,
+        _launchAction = launchAction,
         _url = url;
 
   final String _url;
+  final Future<void> Function(String) _launchAction;
+  final Future<bool> Function(String) _canLaunch;
 
   @override
   Future<void> launch() async {
-    if (await launcher.canLaunch(_url)) {
-      await launcher.launch(_url);
+    if (await _canLaunch(_url)) {
+      await _launchAction(_url);
     }
   }
 }
---
+++ test/unit/sources/data/services/url_launcher/safe_url_launcher_test.dart
@@ -0,0 +1,88 @@
+import 'package:colored/sources/data/services/url_launcher/safe_url_launcher.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockLaunchAction extends Mock {
+  Future<void> call(String url);
+}
+
+class MockCanLaunchCheck extends Mock {
+  Future<bool> call(String url);
+}
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  Future<void> Function(String) launchAction;
+  Future<bool> Function(String) canLaunchCheck;
+
+  setUp(() {
+    launchAction = MockLaunchAction();
+    canLaunchCheck = MockCanLaunchCheck();
+  });
+
+  tearDown(() {
+    launchAction = null;
+    canLaunchCheck = null;
+  });
+
+  group("Given a SafeUrlLauncher", () {
+    group("when constructed", () {
+      test("an assertion error should throw if url is null", () {
+        expect(
+              () => SafeUrlLauncher(url: null),
+          throwsA(isA<AssertionError>()),
+        );
+      });
+
+      test("an assertion error should throw if launchAction is null", () {
+        expect(
+              () => SafeUrlLauncher(url: "test", launchAction: null),
+          throwsA(isA<AssertionError>()),
+        );
+      });
+
+      test("an assertion error should throw if launchAction is null", () {
+        expect(
+              () => SafeUrlLauncher(url: "test", canLaunchCheck: null),
+          throwsA(isA<AssertionError>()),
+        );
+      });
+    });
+  });
+
+  group("Given a SafeUrlLauncher with a valid URL", () {
+    group("when launch is called", () {
+      test("then the launchAction function is called", () async {
+        const url = "https://google.com";
+        when(canLaunchCheck(url)).thenAnswer((realInvocation) async => true);
+        final urlLauncher = SafeUrlLauncher(
+          url: url,
+          launchAction: launchAction,
+          canLaunchCheck: canLaunchCheck,
+        );
+
+        await urlLauncher.launch();
+
+        verify(launchAction.call(url));
+      });
+    });
+  });
+
+  group("Given a SafeUrlLauncher with an invalid URL", () {
+    group("then launch is called", () {
+      test("then the launchAction function is not called", () async {
+        const url = "test";
+        when(canLaunchCheck(url)).thenAnswer((realInvocation) async => false);
+        final urlLauncher = SafeUrlLauncher(
+          url: url,
+          launchAction: launchAction,
+          canLaunchCheck: canLaunchCheck,
+        );
+
+        await urlLauncher.launch();
+
+        verifyNever(launchAction.call(any));
+      });
+    });
+  });
+}
