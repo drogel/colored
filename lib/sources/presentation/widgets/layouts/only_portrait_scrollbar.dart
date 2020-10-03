f9d4b204d64a9752ff89aa3b7d56d90b1f427b8b
Diego Rogel <50502687+drogel@users.noreply.github.com>
2020-10-03 15:48:01 +0200

    fix: removes scrollbar on landscape view on devices with horizontal safe area (#84)

---
+++ lib/sources/presentation/widgets/layouts/only_portrait_scrollbar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class OnlyPortraitScrollbar extends StatelessWidget {
+  const OnlyPortraitScrollbar({
+    @required this.child,
+    Key key,
+  })  : assert(child != null),
+        super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final isLandscape = _isDeviceInLandscapeOrientation(context);
+    final hasHorizontalSafeArea = _hasDeviceHorizontalSafeArea(context);
+
+    if (hasHorizontalSafeArea && isLandscape) {
+      return child;
+    } else {
+      return Scrollbar(child: child);
+    }
+  }
+
+  bool _isDeviceInLandscapeOrientation(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    return orientation == Orientation.landscape;
+  }
+
+  bool _hasDeviceHorizontalSafeArea(BuildContext context) {
+    final safeArea = MediaQuery.of(context).viewPadding;
+    return safeArea.right != 0 || safeArea.left != 0;
+  }
+}
