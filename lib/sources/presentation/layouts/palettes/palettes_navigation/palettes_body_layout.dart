6dce1f6dea70795a99dd0f1d18df9d286834c72d
drogel <diegorogel3d@hotmail.com>
2020-10-05 18:03:52 +0200

    fix: fixes back button not going back in Android and web for palette details page

--- lib/sources/presentation/layouts/palettes/palettes_navigation/palettes_body_layout.dart
+++ lib/sources/presentation/layouts/palettes/palettes_navigation/palettes_body_layout.dart
@@ -1,5 +1,6 @@
 import 'package:colored/sources/domain/data_models/palettes_navigation_selection.dart';
 import 'package:colored/sources/domain/view_models/palettes/palettes_navigation/palettes_navigation_data.dart';
+import 'package:colored/sources/presentation/layouts/palettes/palette_detail/palette_detail_layout.dart';
 import 'package:colored/sources/presentation/layouts/palettes/palettes_list/palettes_list_layout.dart';
 import 'package:colored/sources/presentation/widgets/animations/transition_switcher.dart';
 import 'package:colored/sources/presentation/widgets/animations/page_body_switcher.dart';
@@ -15,9 +16,9 @@ class PalettesBodyLayout extends StatelessWidget {
       reverse: selection == PalettesNavigationSelection.list.index,
       type: PageTransitionType.sharedZAxis,
       currentIndex: selection,
-      children: [
-        const PalettesListLayout(),
-        Container(color: Colors.blue),
+      children: const [
+        PalettesListLayout(),
+        PaletteDetailLayout(),
       ],
     );
   }
