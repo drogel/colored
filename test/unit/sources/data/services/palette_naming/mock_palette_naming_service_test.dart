39a1614ff57b6162d6ccb8bd51fcdafe4daf5d15
drogel <diegorogel3d@hotmail.com>
2020-10-07 22:17:18 +0200

    fix: lowecase hex color codes in palette detail page

--- test/unit/sources/data/services/palette_naming/mock_palette_naming_service_test.dart
+++ test/unit/sources/data/services/palette_naming/mock_palette_naming_service_test.dart
@@ -1,37 +1,42 @@
 import 'package:colored/sources/data/network_client/response_status.dart';
 import 'package:colored/sources/data/services/palette_naming/mock_palette_naming_service.dart';
+import 'package:colored/sources/data/services/palette_naming/palette_naming_response.dart';
 import 'package:colored/sources/data/services/palette_naming/palette_naming_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   PaletteNamingService mockService;
+  PaletteNamingResponse response;
 
-  setUp(() {
+  setUp(() async {
     mockService = const MockPaletteNamingService();
+    response = await mockService.getNaming(hexColors: []);
   });
 
   tearDown(() {
     mockService = null;
+    response = null;
   });
 
   group("Given a MockPaletteNamingService", () {
     group("when getNaming is called", () {
       test("then a response status ok is retrieved", () async {
-        final actual = await mockService.getNaming(hexColors: []);
-        expect(actual.status, ResponseStatus.ok);
+        expect(response.status, ResponseStatus.ok);
       });
 
       test("then color amount from sample palette is retrieved", () async {
-        final actual = await mockService.getNaming(hexColors: []);
-        expect(actual.results.length, 5);
+        expect(response.results.length, 5);
       });
 
       test("then the sample colors are retrieved", () async {
-        final actual = await mockService.getNaming(hexColors: []);
-        expect(actual.results.first.name, "Lead");
-        expect(actual.results.first.hex, "#212121");
-        expect(actual.results.first.r, 33);
+        expect(response.results.first.name, "Lead");
+        expect(response.results.first.hex, "#212121");
+        expect(response.results.first.r, 33);
+      });
+
+      test("then the named colors have uppercase hex codes", () async {
+        expect(response.results[2].hex, "#FF0011");
       });
     });
   });
