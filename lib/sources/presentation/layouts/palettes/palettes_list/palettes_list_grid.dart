be19f19bd279907dc8d84508eb09c5b6c1c019c0
drogel <diegorogel3d@hotmail.com>
2020-10-07 16:51:04 +0200

    feat: adds palette detail body layout

--- lib/sources/presentation/layouts/palettes/palettes_list/palettes_list_grid.dart
+++ lib/sources/presentation/layouts/palettes/palettes_list/palettes_list_grid.dart
@@ -1,6 +1,7 @@
 import 'package:colored/sources/common/extensions/hex_color.dart';
 import 'package:colored/sources/domain/data_models/palette.dart';
 import 'package:colored/sources/domain/data_models/palettes_navigation_selection.dart';
+import 'package:colored/sources/domain/view_models/palettes/palette_detail/palette_detail_data.dart';
 import 'package:colored/sources/domain/view_models/palettes/palettes_navigation/palettes_navigation_data.dart';
 import 'package:colored/sources/presentation/widgets/cards/palette_card.dart';
 import 'package:colored/sources/presentation/widgets/layouts/responsive_grid.dart';
@@ -30,10 +31,15 @@ class PalettesListGrid extends StatelessWidget {
               (palette) => PaletteCard(
                 colors: palette.hexCodes.map(HexColor.fromHex).toList(),
                 title: palette.name,
-                onPressed: (_) => PalettesNavigationData.of(context)
-                    .onNavigation(PalettesNavigationSelection.detail.index),
+                onPressed: (_) => _onPaletteSelected(context, palette.hexCodes),
               ),
             )
             .toList(),
       );
+
+  void _onPaletteSelected(BuildContext context, List<String> hexCodes) {
+    final destination = PalettesNavigationSelection.detail.index;
+    PalettesNavigationData.of(context).onNavigation(destination);
+    PaletteDetailData.of(context).onPaletteSelected(hexCodes);
+  }
 }
