dadfd0dce061aaed5adeec5d587c00a4b0a6cbd0
drogel <diegorogel3d@hotmail.com>
2020-10-07 16:33:28 +0200

    feat: adds basic scheme for palette detail presentation layer

--- lib/sources/presentation/layouts/palettes/palette_detail/palette_detail_layout.dart
+++ lib/sources/presentation/layouts/palettes/palette_detail/palette_detail_layout.dart
@@ -1,5 +1,7 @@
-import 'package:colored/sources/domain/data_models/palettes_navigation_selection.dart';
-import 'package:colored/sources/domain/view_models/palettes/palettes_navigation/palettes_navigation_data.dart';
+import 'package:colored/sources/domain/view_models/palettes/palette_detail/palette_detail_data.dart';
+import 'package:colored/sources/domain/view_models/palettes/palette_detail/palette_detail_state.dart';
+import 'package:colored/sources/presentation/layouts/colors/names_list/names_list_grid.dart';
+import 'package:colored/sources/presentation/widgets/containers/background_container.dart';
 import 'package:flutter/material.dart';
 
 class PaletteDetailLayout extends StatelessWidget {
@@ -7,15 +9,15 @@ class PaletteDetailLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final navigation = PalettesNavigationData.of(context);
-    return WillPopScope(
-      onWillPop: () => _onBackPressed(navigation),
-      child: Container(color: Colors.amber),
-    );
-  }
-
-  Future<bool> _onBackPressed(PalettesNavigationData navigation) async {
-    navigation.onNavigation(PalettesNavigationSelection.list.index);
-    return false;
+    final state = PaletteDetailData.of(context).state;
+    switch (state.runtimeType) {
+      case Failed:
+        return const BackgroundContainer();
+      case PaletteFound:
+        final foundState = state as PaletteFound;
+        return NamesListGrid(namedColors: foundState.namedColors);
+      default:
+        return const BackgroundContainer();
+    }
   }
 }
