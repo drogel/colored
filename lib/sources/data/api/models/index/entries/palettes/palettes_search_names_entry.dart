f8aee3df582336d2f9bacae6e910df03083f8676
drogel <diegorogel3d@hotmail.com>
2021-05-17 22:14:25 +0200

    feat(api-client): adds parsing for palettes search names

--- lib/sources/data/api/models/index/entries/palettes/palettes_search_names_entry.dart
+++ lib/sources/data/api/models/index/entries/palettes/palettes_search_names_entry.dart
@@ -1,11 +1,11 @@
 import 'package:colored/sources/data/api/models/index/entries/api_index_entry.dart';
 
-class PalettesSearchNamesEntry implements ApiIndexEntry {
-  const PalettesSearchNamesEntry({required this.endpoint, required this.title});
+class PalettesSearchNamesEntry extends ApiIndexEntry {
+  const PalettesSearchNamesEntry({
+    required Uri? endpoint,
+    required String title,
+  }) : super(title: title, endpoint: endpoint);
 
-  @override
-  final Uri endpoint;
-
-  @override
-  final String title;
+  PalettesSearchNamesEntry.fromJson(Map<String, dynamic> json)
+      : super.fromJson(json);
 }
