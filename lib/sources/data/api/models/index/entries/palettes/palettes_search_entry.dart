09557de8d2f1860c317d20aa1b91ea682f56a6c2
drogel <diegorogel3d@hotmail.com>
2021-05-17 22:30:57 +0200

    feat(api-client): adds parsing for palettes search entry

--- lib/sources/data/api/models/index/entries/palettes/palettes_search_entry.dart
+++ lib/sources/data/api/models/index/entries/palettes/palettes_search_entry.dart
@@ -1,11 +1,23 @@
 import 'package:colored/sources/data/api/models/index/entries/api_index_entry.dart';
+import 'package:colored/sources/data/api/models/index/entries/palettes/palettes_search_names_entry.dart';
 
-class PalettesSearchEntry implements ApiIndexEntry {
-  const PalettesSearchEntry({required this.endpoint, required this.title});
+class PalettesSearchEntry extends ApiIndexEntry {
+  const PalettesSearchEntry({
+    required String title,
+    Uri? endpoint,
+    this.names,
+  }) : super(title: title, endpoint: endpoint);
 
-  @override
-  final Uri endpoint;
+  PalettesSearchEntry.fromJson(Map<String, dynamic> json)
+      : names = getNamesIfAny(json),
+        super.fromJson(json);
 
-  @override
-  final String title;
+  final PalettesSearchNamesEntry? names;
+
+  static PalettesSearchNamesEntry? getNamesIfAny(Map<String, dynamic> json) =>
+      ApiIndexEntry.getChildEntryAtIndexIfAny(
+        0,
+        json: json,
+        entryBuilder: (childMap) => PalettesSearchNamesEntry.fromJson(childMap),
+      );
 }
---
+++ test/unit/sources/data/api/models/index/palettes/palettes_search_entry_test.dart
@@ -0,0 +1,46 @@
+import 'package:colored/sources/data/api/models/index/entries/palettes/palettes_search_entry.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../test_helpers/api_index_entry_test_runner.dart';
+import '../../../test_helpers/entry_tester.dart';
+import 'palettes_search_names_entry_test.dart';
+
+class PalettesSearchEntryTester extends EntryTester<PalettesSearchEntry> {
+  const PalettesSearchEntryTester(ApiIndexEntryTestRunner testRunner)
+      : super(testRunner);
+
+  @override
+  Map<String, dynamic> get testMap => {
+        "title": "palettes_search",
+        "entries": [
+          {
+            "title": "palettes_search_names",
+            "endpoint": "https://test.com/palettes/search/names?name={name}"
+          }
+        ]
+      };
+
+  @override
+  void assertExpectations(PalettesSearchEntry entry) {
+    testRunner.assertExpectations(entry, expectedTitle: "palettes_search");
+    final searchNamesTester = PalettesSearchNamesEntryTester(testRunner);
+    final searchNamesEntry = entry.names;
+    assertChildEntryExpectations(
+      entryTester: searchNamesTester,
+      entry: searchNamesEntry,
+    );
+  }
+}
+
+void main() {
+  group("Given a $PalettesSearchEntry", () {
+    group("when constructed from a JSON", () {
+      test("then values are correctly parsed", () {
+        const testRunner = ApiIndexEntryTestRunner();
+        const tester = PalettesSearchEntryTester(testRunner);
+        final entry = PalettesSearchEntry.fromJson(tester.testMap);
+        tester.assertExpectations(entry);
+      });
+    });
+  });
+}
