95cd8f89e589e08cce71aaed9ecb305a25b024e7
drogel <diegorogel3d@hotmail.com>
2021-05-18 19:19:56 +0200

    feat(api-client): adds palette entry parsing

--- lib/sources/data/api/models/index/entries/palettes/palettes_entry.dart
+++ lib/sources/data/api/models/index/entries/palettes/palettes_entry.dart
@@ -1,11 +1,23 @@
 import 'package:colored/sources/data/api/models/index/entries/api_index_entry.dart';
+import 'package:colored/sources/data/api/models/index/entries/palettes/palettes_search_entry.dart';
 
-class PalettesEntry implements ApiIndexEntry {
-  const PalettesEntry({required this.endpoint, required this.title});
+class PalettesEntry extends ApiIndexEntry {
+  const PalettesEntry({
+    required Uri? endpoint,
+    required String title,
+    this.search,
+  }) : super(endpoint: endpoint, title: title);
 
-  @override
-  final Uri endpoint;
+  PalettesEntry.fromJson(Map<String, dynamic> json)
+      : search = getSearchIfAny(json),
+        super.fromJson(json);
 
-  @override
-  final String title;
+  final PalettesSearchEntry? search;
+
+  static PalettesSearchEntry? getSearchIfAny(Map<String, dynamic> json) =>
+      ApiIndexEntry.getChildEntryAtIndexIfAny(
+        0,
+        json: json,
+        entryBuilder: (childMap) => PalettesSearchEntry.fromJson(childMap),
+      );
 }
