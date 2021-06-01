9a315956f8964d078549325b182ed6cd268060e0
Diego Rogel <diego.rogel@nomasystems.com>
2021-06-01 19:39:14 +0200

    feat(api-client): adds index parsing for colors suggestions

--- lib/sources/data/api/models/index/entries/suggestions/colors/colors_suggestions_entry.dart
+++ lib/sources/data/api/models/index/entries/suggestions/colors/colors_suggestions_entry.dart
@@ -1,11 +1,40 @@
 import 'package:colored/sources/data/api/models/index/entries/api_index_entry.dart';
+import 'package:colored/sources/data/api/models/index/entries/suggestions/colors/colors_suggestions_search_entry.dart';
+import 'package:colored/sources/data/api/models/index/entries/suggestions/colors/random_colors_suggestions_entry.dart';
 
-class ColorsSuggestionsEntry implements ApiIndexEntry {
-  const ColorsSuggestionsEntry({required this.endpoint, required this.title});
+class ColorsSuggestionsEntry extends ApiIndexEntry {
+  const ColorsSuggestionsEntry({
+    required String title,
+    Uri? endpoint,
+    this.random,
+    this.search,
+  }) : super(title: title, endpoint: endpoint);
 
-  @override
-  final Uri endpoint;
+  ColorsSuggestionsEntry.fromJson(Map<String, dynamic> json)
+      : random = getRandomIfAny(json),
+        search = getSearchIfAny(json),
+        super.fromJson(json);
 
-  @override
-  final String title;
+  final RandomColorsSuggestionsEntry? random;
+  final ColorsSuggestionsSearchEntry? search;
+
+  static RandomColorsSuggestionsEntry? getRandomIfAny(
+    Map<String, dynamic> json,
+  ) =>
+      ApiIndexEntry.getChildEntryAtIndexIfAny(
+        0,
+        json: json,
+        entryBuilder: (childMap) =>
+            RandomColorsSuggestionsEntry.fromJson(childMap),
+      );
+
+  static ColorsSuggestionsSearchEntry? getSearchIfAny(
+    Map<String, dynamic> json,
+  ) =>
+      ApiIndexEntry.getChildEntryAtIndexIfAny(
+        1,
+        json: json,
+        entryBuilder: (childMap) =>
+            ColorsSuggestionsSearchEntry.fromJson(childMap),
+      );
 }
