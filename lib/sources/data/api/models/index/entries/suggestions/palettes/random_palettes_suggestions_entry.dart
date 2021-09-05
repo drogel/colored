f97062671cca9490ed1ca26e218a94cb2df2e934
Diego Rogel <diego.rogel@nomasystems.com>
2021-09-05 22:09:03 +0200

    feat(api-client): adds index for palettes suggestions

--- lib/sources/data/api/models/index/entries/suggestions/palettes/random_palettes_suggestions_entry.dart
+++ lib/sources/data/api/models/index/entries/suggestions/palettes/random_palettes_suggestions_entry.dart
@@ -1,14 +1,11 @@
 import 'package:colored/sources/data/api/models/index/entries/api_index_entry.dart';
 
-class RandomPalettesSuggestionsEntry implements ApiIndexEntry {
+class RandomPalettesSuggestionsEntry extends ApiIndexEntry {
   const RandomPalettesSuggestionsEntry({
-    required this.endpoint,
-    required this.title,
-  });
+    required String title,
+    Uri? endpoint,
+  }) : super(title: title, endpoint: endpoint);
 
-  @override
-  final Uri endpoint;
-
-  @override
-  final String title;
+  RandomPalettesSuggestionsEntry.fromJson(Map<String, dynamic> json)
+      : super.fromJson(json);
 }
