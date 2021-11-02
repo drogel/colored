9331fc18fe9ebfcf27e8a21470c3719954536aec
Diego Rogel <diegorogel3d@hotmail.com>
2021-11-02 13:40:08 +0100

    feat(pagination): implements a first approach towards a paginator class

---
+++ lib/sources/data/pagination/list_paginator.dart
@@ -0,0 +1,41 @@
+import 'package:colored/sources/data/pagination/page_info.dart';
+import 'package:colored/sources/data/pagination/list_page.dart';
+import 'package:colored/sources/data/pagination/paginator.dart';
+
+class ListPaginator<T> implements Paginator<T> {
+  const ListPaginator();
+
+  @override
+  ListPage<T> paginate(List<T> items, {required PageInfo pageInfo}) {
+    final startIndex = pageInfo.startIndex;
+    final pageSize = pageInfo.size;
+    final pageIndex = pageInfo.pageIndex;
+    final sublistStartIndex = pageSize * (pageIndex - startIndex);
+    final listEndIndex = items.length;
+    final sublistEndIndex = _getSublistEndIndex(
+      sublistStartIndex: sublistStartIndex,
+      pageSize: pageSize,
+      listEndIndex: listEndIndex,
+    );
+    final totalPages = (items.length / pageSize).ceil();
+    final sublist = items.sublist(sublistStartIndex, sublistEndIndex);
+    return ListPage<T>(
+      currentItemCount: sublist.length,
+      itemsPerPage: pageSize,
+      startIndex: startIndex,
+      totalItems: items.length,
+      pageIndex: pageIndex,
+      totalPages: totalPages,
+      items: sublist,
+    );
+  }
+
+  int _getSublistEndIndex({
+    required int sublistStartIndex,
+    required int pageSize,
+    required int listEndIndex,
+  }) {
+    final maxSublistStartIndex = sublistStartIndex + pageSize;
+    return maxSublistStartIndex.clamp(sublistStartIndex, listEndIndex);
+  }
+}
---
+++ lib/sources/data/pagination/page_info.dart
@@ -0,0 +1,11 @@
+class PageInfo {
+  const PageInfo({
+    required this.startIndex,
+    required this.size,
+    required this.pageIndex,
+  });
+
+  final int startIndex;
+  final int size;
+  final int pageIndex;
+}
