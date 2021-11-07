b39a5242415dfddb5d6709fddfcb82da9637f2a7
Diego Rogel <diegorogel3d@hotmail.com>
2021-11-07 22:23:18 +0100

    feat(pagination): adds base paginated names service

--- lib/sources/data/services/names/paginated_color_names_service.dart
+++ lib/sources/data/services/names/paginated_color_names_service.dart
@@ -1,34 +1,19 @@
-import 'package:colored/sources/data/pagination/page_info.dart';
-import 'package:colored/sources/data/pagination/list_page.dart';
 import 'package:colored/sources/data/pagination/paginator.dart';
+import 'package:colored/sources/data/services/names/base_paginated_names_service.dart';
 import 'package:colored/sources/data/services/names/names_service.dart';
-import 'package:colored/sources/data/services/names/paginated_names_service.dart';
 import 'package:colored/sources/domain/data_models/named_color.dart';
 
-class PaginatedColorNamesService implements PaginatedNamesService<NamedColor> {
+class PaginatedColorNamesService
+    extends BasePaginatedNamesService<NamedColor, String> {
   const PaginatedColorNamesService({
     required NamesService<String> namesService,
     required Paginator<NamedColor> paginator,
-  })  : _namesService = namesService,
-        _paginator = paginator;
+  }) : super(
+          namesService: namesService,
+          paginator: paginator,
+          mapper: _convertToNamedColor,
+        );
 
-  final NamesService<String> _namesService;
-  final Paginator<NamedColor> _paginator;
-
-  @override
-  Future<ListPage<NamedColor>> fetchContainingSearch(
-    String searchString, {
-    required PageInfo pageInfo,
-  }) async {
-    final namesMap = await _namesService.fetchContainingSearch(searchString);
-    final namedColors = namesMap.entries.map(_convertToNamedColor).toList();
-    final pagedNamedColors = _paginator.paginate(
-      namedColors,
-      pageInfo: pageInfo,
-    );
-    return pagedNamedColors;
-  }
-
-  NamedColor _convertToNamedColor(MapEntry<String, dynamic> entry) =>
+  static NamedColor _convertToNamedColor(MapEntry<String, dynamic> entry) =>
       NamedColor.fromMapEntry(entry);
 }
