42a8c3477860fb9fdab11bbfe24bf94ef8783ee7
Diego Rogel <diegorogel3d@hotmail.com>
2021-11-14 19:38:20 +0100

    feat(api-closest): removes unused classes

--- test/unit/sources/domain/view_models/on_boarding/on_boarding_device_orientation_service_test.dart
+++ test/unit/sources/domain/view_models/on_boarding/on_boarding_device_orientation_service_test.dart
@@ -1,11 +1,22 @@
 import 'dart:async';
 
 import 'package:colored/sources/data/services/device_orientation/device_orientation_service.dart';
-import 'package:colored/sources/data/services/local_storage/mock_local_storage.dart';
+import 'package:colored/sources/data/services/local_storage/local_storage.dart';
 import 'package:colored/sources/domain/view_models/on_boarding/on_boarding_state.dart';
 import 'package:colored/sources/domain/view_models/on_boarding/on_boarding_view_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+
+class MockLocalStorage implements LocalStorage {
+  const MockLocalStorage();
+
+  @override
+  Future<bool?> getBool({required String key}) async => false;
+
+  @override
+  Future<bool> storeBool({required String key, required bool value}) async =>
+      true;
+}
 
 class MockDeviceOrientationService extends Mock
     implements DeviceOrientationService {}
