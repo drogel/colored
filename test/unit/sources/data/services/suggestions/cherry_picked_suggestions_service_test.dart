f0192744fdc1519ee1d6172a92617024de70cf7c
Diego Rogel <diegorogel3d@hotmail.com>
2021-11-15 18:57:03 +0100

    feat(api-suggestions): adds named colors paginated suggestions service

--- test/unit/sources/data/services/suggestions/cherry_picked_suggestions_service_test.dart
+++ test/unit/sources/data/services/suggestions/cherry_picked_suggestions_service_test.dart
@@ -1,40 +1,10 @@
 import 'package:colored/sources/data/services/data_loader/data_loader.dart';
-import 'package:colored/sources/data/services/int_generator/int_generator.dart';
 import 'package:colored/sources/data/services/list_picker/string_list_picker.dart';
 import 'package:colored/sources/data/services/suggestions/cherry_picked_suggestions_service.dart';
 import 'package:colored/sources/data/services/suggestions/suggestions_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class ColorSuggestionsLoaderStub implements DataLoader<String> {
-  static const mockSuggestions = {
-    "111111": "First",
-    "222222": "Second",
-    "333333": "Third",
-    "444444": "Fourth",
-  };
-
-  @override
-  Future<Map<String, String>> load() async => mockSuggestions;
-}
-
-class PaletteSuggestionsLoaderStub implements DataLoader<List<String>> {
-  static const mockSuggestions = <String, List<String>>{
-    "First": [],
-    "Second": ["000000", "ffffff"],
-    "Third": [],
-    "Fourth": ["000000"],
-  };
-
-  @override
-  Future<Map<String, List<String>>> load() async => mockSuggestions;
-}
-
-class RandomGeneratorStub implements IntGenerator {
-  static const stubbedIntList = [1, 3];
-
-  @override
-  List<int> generate({int? max, int? length}) => stubbedIntList;
-}
+import 'suggestions_service_test_helper.dart';
 
 void main() {
   group("Given a CherryPickedSuggestionsService with a color data loader", () {
---
+++ test/unit/sources/data/services/suggestions/named_colors_suggestions_service_test.dart
@@ -0,0 +1,47 @@
+import 'package:colored/sources/data/pagination/list_page.dart';
+import 'package:colored/sources/data/pagination/page_info.dart';
+import 'package:colored/sources/data/services/data_loader/data_loader.dart';
+import 'package:colored/sources/data/services/list_picker/string_list_picker.dart';
+import 'package:colored/sources/data/services/suggestions/cherry_picked_suggestions_service.dart';
+import 'package:colored/sources/data/services/suggestions/named_colors_paginated_suggestions_service.dart';
+import 'package:colored/sources/domain/data_models/named_color.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'suggestions_service_test_helper.dart';
+
+void main() {
+  group("Given a $NamedColorsPaginatedSuggestionsService", () {
+    late NamedColorsPaginatedSuggestionsService suggestionsService;
+    late DataLoader<String> suggestionsDataLoader;
+
+    setUp(() {
+      suggestionsDataLoader = ColorSuggestionsLoaderStub();
+      suggestionsService = NamedColorsPaginatedSuggestionsService(
+        suggestionsService: CherryPickedSuggestionsService<String>(
+          dataLoader: suggestionsDataLoader,
+          listPicker: StringListPicker(
+            intGenerator: RandomGeneratorStub(),
+          ),
+        ),
+      );
+    });
+
+    group("when fetchRandom is called", () {
+      test("then the expected $NamedColor list is retrieved", () async {
+        const pageInfo = PageInfo(startIndex: 1, size: 2, pageIndex: 1);
+        final actual = await suggestionsService.fetchRandom(pageInfo: pageInfo);
+        const expected = [
+          NamedColor(name: "Second", hex: "#222222"),
+          NamedColor(name: "Fourth", hex: "#444444"),
+        ];
+		final actualPage = actual;
+		if (actualPage == null) {
+			fail("Expected a non-null $ListPage");
+		}
+        expect(actualPage.items, expected);
+        expect(actualPage.currentItemCount, 2);
+        expect(actualPage.itemsPerPage, 2);
+      });
+    });
+  });
+}
