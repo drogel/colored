bc1050d5d792006eea32a5c5979500bc6d3c6ca5
Diego Rogel <diegorogel3d@hotmail.com>
2021-11-15 15:02:07 +0100

    feat(palette-detail-api): removes unused files

--- lib/sources/data/services/naming/naming_response.dart
+++
@@ -1,9 +0,0 @@
-import 'package:colored/sources/data/network_client/response_status.dart';
-import 'package:colored/sources/data/network_client/response_wrapper.dart';
-import 'package:colored/sources/domain/data_models/naming_result.dart';
-
-class NamingResponse extends ResponseWrapper {
-  const NamingResponse(ResponseStatus status, {this.result}) : super(status);
-
-  final NamingResult? result;
-}
--- lib/sources/data/services/palette_naming/palette_naming_service.dart
+++
@@ -1,5 +0,0 @@
-import 'package:colored/sources/data/services/palette_naming/palette_naming_response.dart';
-
-abstract class PaletteNamingService {
-  Future<PaletteNamingResponse> getNaming({required List<String> hexColors});
-}
