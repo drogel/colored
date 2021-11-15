2e431f9bf2656a944e76cd6786063c31cf0934b9
Diego Rogel <diegorogel3d@hotmail.com>
2021-11-15 21:06:16 +0100

    feat(api-suggestions): integrates color api suggestions

--- lib/sources/domain/view_models/colors/color_suggestions/color_suggestions_view_model.dart
+++ lib/sources/domain/view_models/colors/color_suggestions/color_suggestions_view_model.dart
@@ -1,38 +1,41 @@
 import 'dart:async';
 
-import 'package:colored/sources/data/services/suggestions/suggestions_service.dart';
+import 'package:colored/sources/data/pagination/page_info.dart';
+import 'package:colored/sources/data/services/suggestions/paginated_suggestions_service.dart';
 import 'package:colored/sources/domain/data_models/named_color.dart';
 import 'package:colored/sources/domain/view_models/colors/color_suggestions/color_suggestions_state.dart';
 
-const _kSuggestionsLength = 15;
+const _kSuggestionsLength = 20;
+const _kStartIndex = 1;
+
+const _kPageInfo = PageInfo(
+  startIndex: _kStartIndex,
+  size: _kSuggestionsLength,
+  pageIndex: _kStartIndex,
+);
 
 class ColorSuggestionsViewModel {
   const ColorSuggestionsViewModel({
     required StreamController<ColorSuggestionsState> stateController,
-    required SuggestionsService<String> suggestionsService,
-  })   : _stateController = stateController,
+    required PaginatedSuggestionsService<NamedColor> suggestionsService,
+  })  : _stateController = stateController,
         _suggestionsService = suggestionsService;
 
   final StreamController<ColorSuggestionsState> _stateController;
-  final SuggestionsService<String> _suggestionsService;
+  final PaginatedSuggestionsService<NamedColor> _suggestionsService;
 
   Stream<ColorSuggestionsState> get stateStream => _stateController.stream;
 
   ColorSuggestionsState get initialState => const Loading();
 
   Future<void> init() async {
-    final map = await _suggestionsService.fetchSuggestions(_kSuggestionsLength);
-    final namedColors = map.entries.map(_convertToNamedColor).toList();
-
-    if (namedColors.isEmpty) {
+    final page = await _suggestionsService.fetchRandom(pageInfo: _kPageInfo);
+    if (page == null || page.items.isEmpty) {
       _stateController.sink.add(const Failed());
     } else {
-      _stateController.sink.add(ColorSuggestionsFound(namedColors));
+      _stateController.sink.add(ColorSuggestionsFound(page.items));
     }
   }
 
   void dispose() => _stateController.close();
-
-  NamedColor _convertToNamedColor(MapEntry<String, String> entry) =>
-      NamedColor.fromMapEntry(entry);
 }
