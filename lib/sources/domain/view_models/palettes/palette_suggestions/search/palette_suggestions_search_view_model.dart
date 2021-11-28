b6e7d83411a860a5c53a34a3db1d41d961a6a0fa
Diego Rogel <diegorogel3d@hotmail.com>
2021-11-28 20:27:50 +0100

    feat(autocomplete): adds palette suggestions search view model

---
+++ lib/sources/domain/view_models/palettes/palette_suggestions/search/palette_suggestions_search_view_model.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:colored/sources/data/pagination/page_info.dart';
+import 'package:colored/sources/data/services/names/paginated_names_service.dart';
+import 'package:colored/sources/domain/data_models/palette.dart';
+import 'package:colored/sources/domain/view_models/base/names/base_names_view_model.dart';
+import 'package:colored/sources/domain/view_models/base/names/names_state.dart';
+import 'package:colored/sources/domain/view_models/palettes/palettes_list/palettes_list_state.dart';
+
+class PaletteSuggestionsSearchViewModel
+    extends BaseNamesListViewModel<Palette> {
+  const PaletteSuggestionsSearchViewModel({
+    required StreamController<NamesListState> stateController,
+    required PaginatedNamesService<Palette> namesService,
+  }) : super(
+          stateController: stateController,
+          namesService: namesService,
+        );
+
+  @override
+  NamesListState buildInitialState() => const Starting();
+
+  @override
+  NamesListState buildSearchFailedState(String searchString) =>
+      NoneFound(search: searchString);
+
+  @override
+  NamesListState? buildSearchPendingState(String searchString) => null;
+
+  @override
+  NamesListState buildSearchSuccessState(
+    String searchString, {
+    required PageInfo pageInfo,
+    required List<Palette> items,
+  }) =>
+      Found(items, search: searchString, pageInfo: pageInfo);
+}
---
+++ test/unit/sources/domain/view_models/palettes/palette_suggestions/search/palette_suggestions_search_view_model_test.dart
@@ -0,0 +1,377 @@
+import 'dart:async';
+
+import 'package:colored/sources/data/pagination/list_page.dart';
+import 'package:colored/sources/data/pagination/list_paginator.dart';
+import 'package:colored/sources/data/pagination/page_info.dart';
+import 'package:colored/sources/data/services/names/names_service.dart';
+import 'package:colored/sources/data/services/names/paginated_names_service.dart';
+import 'package:colored/sources/data/services/names/paginated_palette_names_service.dart';
+import 'package:colored/sources/domain/data_models/palette.dart';
+import 'package:colored/sources/domain/view_models/base/names/names_state.dart';
+import 'package:colored/sources/domain/view_models/colors/color_suggestions/search/color_suggestions_search_view_model.dart';
+import 'package:colored/sources/domain/view_models/palettes/palette_suggestions/search/palette_suggestions_search_view_model.dart';
+import 'package:colored/sources/domain/view_models/palettes/palettes_list/palettes_list_state.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNamesService extends Mock implements NamesService<List<String>> {}
+
+class NamesServiceStub implements NamesService<List<String>> {
+  static const Map<String, List<String>> namesMap = {
+    "testHex": ["testName", "testName2"],
+    "000000": ["000000", "000000"],
+    "000001": ["000001", "000001"],
+    "000002": ["000002", "000002"],
+  };
+
+  @override
+  Future<Map<String, List<String>>> fetchContainingSearch(
+          String searchString) async =>
+      namesMap;
+}
+
+class NamesServiceEmptyStub implements NamesService<List<String>> {
+  @override
+  Future<Map<String, List<String>>> fetchContainingSearch(
+          String searchString) async =>
+      {};
+}
+
+class NamesServiceNullStub implements PaginatedNamesService<Palette> {
+  const NamesServiceNullStub();
+
+  @override
+  Future<ListPage<Palette>?> fetchContainingSearch(
+    String searchString, {
+    required PageInfo pageInfo,
+  }) async =>
+      null;
+}
+
+const testPageInfo = PageInfo(startIndex: 1, size: 1, pageIndex: 1);
+
+const testCurrentPalettes = [
+  Palette(name: "gray", hexCodes: ["#111111", "#111111"]),
+  Palette(name: "red", hexCodes: ["#FF0000", "#FF0000"]),
+];
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  late PaletteSuggestionsSearchViewModel viewModel;
+  late StreamController<NamesListState> stateController;
+  late NamesService<List<String>> namesService;
+
+  setUp(() {
+    stateController = StreamController<NamesListState>();
+    namesService = MockNamesService();
+    viewModel = PaletteSuggestionsSearchViewModel(
+      stateController: stateController,
+      namesService: PaginatedPaletteNamesService(
+        namesService: namesService,
+        paginator: const ListPaginator(),
+      ),
+    );
+  });
+
+  tearDown(() {
+    stateController.close();
+  });
+
+  group("Given a $PaletteSuggestionsSearchViewModel", () {
+    group("when initialState is called", () {
+      test("then a Starting state is received", () {
+        final initialState = viewModel.initialState;
+        expect(initialState, isA<Starting>());
+      });
+    });
+
+    group("when clearSearch is called", () {
+      test("then a Starting state is received", () {
+        stateController.stream.listen(
+          (event) => expect(event, isA<Starting>()),
+        );
+        viewModel.clearSearch();
+      });
+    });
+
+    group("when dispose is called", () {
+      test("then stateController is closed", () {
+        expect(stateController.isClosed, false);
+        viewModel.dispose();
+        expect(stateController.isClosed, true);
+      });
+    });
+
+    group("when stateStream is called", () {
+      test("the stream of the provided state controller is retrieved", () {
+        expect(viewModel.stateStream, stateController.stream);
+      });
+    });
+  });
+
+  group("Given a $PaletteSuggestionsSearchViewModel with stubs", () {
+    setUp(() {
+      stateController = StreamController<NamesListState>();
+      namesService = NamesServiceStub();
+      viewModel = PaletteSuggestionsSearchViewModel(
+        stateController: stateController,
+        namesService: PaginatedPaletteNamesService(
+          namesService: namesService,
+          paginator: const ListPaginator(),
+        ),
+      );
+    });
+
+    tearDown(() {
+      stateController.close();
+    });
+
+    group("when searchNextPage is called", () {
+      test("then a Pending state is not added", () async {
+        await viewModel.searchNextPage(
+          "se",
+          currentItems: testCurrentPalettes,
+          currentPageInfo: testPageInfo,
+        );
+        expect(await viewModel.stateStream.first, isNot(isA<Pending>()));
+      });
+
+      test("then a Found state is added after the first Pending state", () {
+        stateController.stream.skip(1).listen(
+              (event) => expect(event, isA<Found>()),
+            );
+        viewModel.searchNextPage(
+          "red",
+          currentItems: testCurrentPalettes,
+          currentPageInfo: testPageInfo,
+        );
+      });
+
+      test("then the search from the Pending state is retrieved", () {
+        const expected = "se";
+        stateController.stream.listen(
+          (event) => expect(event.search, expected),
+        );
+        viewModel.searchNextPage(
+          expected,
+          currentItems: testCurrentPalettes,
+          currentPageInfo: testPageInfo,
+        );
+      });
+
+      test("then the search from the Found state is retrieved", () {
+        const expected = "search";
+        stateController.stream.skip(1).listen(
+              (event) => expect(event.search, expected),
+            );
+        viewModel.searchNextPage(
+          expected,
+          currentItems: testCurrentPalettes,
+          currentPageInfo: testPageInfo,
+        );
+      });
+
+      group("namedColors are retrieved", () {
+        test("containing the first namedColor from the current colors", () {
+          stateController.stream.skip(1).listen((event) {
+            final found = event as Found;
+            final expected = Palette(
+              name: testCurrentPalettes.first.name,
+              hexCodes: testCurrentPalettes.first.hexCodes,
+            );
+            final actual = found.palettes.first;
+            expect(actual, expected);
+          });
+          viewModel.searchNextPage(
+            "red",
+            currentItems: testCurrentPalettes,
+            currentPageInfo: testPageInfo,
+          );
+        });
+
+        test("containing the first namedColor from the retrieved colors", () {
+          stateController.stream.skip(1).listen((event) {
+            final found = event as Found;
+            const namesMap = NamesServiceStub.namesMap;
+            final paletteName = namesMap.keys.toList()[1];
+            final expected = Palette(
+              name: paletteName,
+              hexCodes: namesMap[paletteName]!
+                  .map((e) => "#${e.toUpperCase()}")
+                  .toList(),
+            );
+            final actual = found.palettes[testCurrentPalettes.length];
+            expect(actual, expected);
+          });
+          viewModel.searchNextPage(
+            "red",
+            currentItems: testCurrentPalettes,
+            currentPageInfo: testPageInfo,
+          );
+        });
+      });
+    });
+
+    group("when startSearch is called", () {
+      test("then a Pending state is not added", () async {
+        await viewModel.startSearch("se");
+        expect(await viewModel.stateStream.first, isNot(isA<Pending>()));
+      });
+
+      test("then Found state is added after the Starting state", () {
+        stateController.stream.skip(1).listen(
+              (event) => expect(event, isA<Found>()),
+            );
+        viewModel.startSearch("red");
+      });
+
+      test("then the search from the Starting state is retrieved", () {
+        const expected = "se";
+        stateController.stream.listen(
+          (event) => expect(event.search, expected),
+        );
+        viewModel.startSearch(expected);
+      });
+
+      test("then the search from the Found state is retrieved", () {
+        const expected = "search";
+        stateController.stream.skip(1).listen(
+              (event) => expect(event.search, expected),
+            );
+        viewModel.startSearch(expected);
+      });
+
+      test("then namedColors are retrieved", () {
+        stateController.stream.skip(1).listen((event) {
+          final found = event as Found;
+          final expected = Palette(
+            name: NamesServiceStub.namesMap.keys.first,
+            hexCodes: NamesServiceStub.namesMap.values.first
+                .map((e) => "#${e.toUpperCase()}")
+                .toList(),
+          );
+          final actual = found.palettes.first;
+          expect(actual, expected);
+        });
+        viewModel.startSearch("red");
+      });
+    });
+  });
+
+  group("Given a $ColorSuggestionsSearchViewModel with empty NamesService", () {
+    setUp(() {
+      stateController = StreamController<NamesListState>();
+      namesService = NamesServiceEmptyStub();
+      viewModel = PaletteSuggestionsSearchViewModel(
+        stateController: stateController,
+        namesService: PaginatedPaletteNamesService(
+          namesService: namesService,
+          paginator: const ListPaginator(),
+        ),
+      );
+    });
+
+    tearDown(() {
+      stateController.close();
+    });
+
+    group("when startSearch is called", () {
+      test("then a Pending state is added", () async {
+        await viewModel.startSearch("se");
+        expect(await viewModel.stateStream.first, isNot(isA<Pending>()));
+      });
+
+      test("then NoneFound is retrieved after the Pending state", () {
+        stateController.stream.skip(1).listen(
+              (event) => expect(event, isA<NoneFound>()),
+            );
+        viewModel.startSearch("red");
+      });
+    });
+
+    group("when searchNextPage is called", () {
+      test("then Pending is not added", () async {
+        await viewModel.searchNextPage(
+          "se",
+          currentItems: testCurrentPalettes,
+          currentPageInfo: testPageInfo,
+        );
+        expect(await viewModel.stateStream.first, isNot(isA<Pending>()));
+      });
+
+      test("then Found is retrieved after the Pending is added", () {
+        stateController.stream.skip(1).listen(
+              (event) => expect(event, isA<Found>()),
+            );
+        viewModel.searchNextPage(
+          "red",
+          currentItems: testCurrentPalettes,
+          currentPageInfo: testPageInfo,
+        );
+      });
+
+      test("then namedColors found are the currentItems", () {
+        stateController.stream.skip(1).listen((event) {
+          final found = event as Found;
+          expect(found.palettes, testCurrentPalettes);
+        });
+        viewModel.searchNextPage(
+          "red",
+          currentItems: testCurrentPalettes,
+          currentPageInfo: testPageInfo,
+        );
+      });
+    });
+  });
+
+  group("Given a $ColorSuggestionsSearchViewModel with an stubs", () {
+    setUp(() {
+      stateController = StreamController<NamesListState>();
+      viewModel = PaletteSuggestionsSearchViewModel(
+        stateController: stateController,
+        namesService: const NamesServiceNullStub(),
+      );
+    });
+
+    tearDown(() {
+      stateController.close();
+    });
+
+    group("when startSearch is called", () {
+      test("then a Pending state is not added", () async {
+        await viewModel.startSearch("se");
+        expect(await viewModel.stateStream.first, isNot(isA<Pending>()));
+      });
+
+      test("then NoneFound is retrieved after the Pending state", () {
+        stateController.stream.skip(1).listen(
+              (event) => expect(event, isA<NoneFound>()),
+            );
+        viewModel.startSearch("red");
+      });
+    });
+
+    group("when searchNextPage is called", () {
+      test("then a Pending state is not added", () async {
+        await viewModel.searchNextPage(
+          "red",
+          currentPageInfo: testPageInfo,
+          currentItems: testCurrentPalettes,
+        );
+        expect(await viewModel.stateStream.first, isNot(isA<Pending>()));
+      });
+
+      test("then NoneFound is retrieved after the Pending state", () {
+        stateController.stream.skip(1).listen(
+              (event) => expect(event, isA<NoneFound>()),
+            );
+        viewModel.searchNextPage(
+          "red",
+          currentPageInfo: testPageInfo,
+          currentItems: testCurrentPalettes,
+        );
+      });
+    });
+  });
+}
