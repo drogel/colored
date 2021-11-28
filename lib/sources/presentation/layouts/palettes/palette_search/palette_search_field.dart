e4b1911f429ab387344498a2545f41c5c8e36ad9
Diego Rogel <50502687+drogel@users.noreply.github.com>
2021-11-28 22:33:09 +0100

    feat(autocomplete): integrates palette search suggestions (#145)

--- lib/sources/presentation/layouts/palettes/palette_search/palette_search_field.dart
+++ lib/sources/presentation/layouts/palettes/palette_search/palette_search_field.dart
@@ -1,6 +1,9 @@
 import 'package:colored/resources/localization/localization.dart';
+import 'package:colored/sources/domain/data_models/palette.dart';
+import 'package:colored/sources/domain/view_models/palettes/palette_suggestions/search/palette_suggestions_search_data.dart';
 import 'package:colored/sources/domain/view_models/palettes/palettes_list/palettes_list_data.dart';
-import 'package:colored/sources/presentation/widgets/text_fields/auto_focusing_search_field.dart';
+import 'package:colored/sources/domain/view_models/palettes/palettes_list/palettes_list_state.dart';
+import 'package:colored/sources/presentation/widgets/text_fields/autocomplete_search_field.dart';
 import 'package:flutter/material.dart';
 
 class PaletteSearchField extends StatelessWidget {
@@ -9,12 +12,28 @@ class PaletteSearchField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final data = PalettesListData.of(context)!;
+    final suggestionsData = PaletteSuggestionsSearchData.of(context)!;
     final localization = Localization.of(context)!.palettes;
-    return AutoFocusingSearchField(
+    return AutocompleteSearchField(
+      autocompleteOptions: _buildSuggestionsOptions(suggestionsData),
       hintText: localization.search,
       onClearPressed: data.onSearchCleared,
       onSubmitted: data.onSearchStarted,
+      onChanged: suggestionsData.onSearchStarted,
       searchText: data.state.search,
     );
+  }
+
+  Iterable<Palette> _buildSuggestionsOptions(
+    PaletteSuggestionsSearchData data,
+  ) {
+    final suggestionsState = data.state;
+    switch (suggestionsState.runtimeType) {
+      case Found:
+        final foundState = suggestionsState as Found;
+        return foundState.palettes;
+      default:
+        return [];
+    }
   }
 }
